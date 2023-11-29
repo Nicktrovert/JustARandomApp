4dd76577d0339636647f1c7dced9c335b0045ca5
Nick Helmers <yolorolo2007@gmail.com>
2023-11-29 18:28:56 +0100

    b

---
+++ JustARandomApp/Code/ApiHandlers.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.JSInterop;
+using Newtonsoft.Json;
+
+namespace JustARandomApp.Code
+{
+    public class RanCatApi
+    {
+        public string id { get; set; }
+        public string url { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+    public class RanCatApiHandler
+    {
+        private HttpClient httpC;
+        private HttpResponseMessage httpRM;
+        private RanCatApi ranCatApi = new RanCatApi();
+
+        public RanCatApiHandler()
+        {
+            httpC = new HttpClient();
+            httpRM = new HttpResponseMessage();
+        }
+
+        public async Task<string> GetImage()
+        {
+            httpRM = await httpC.GetAsync("https://api.thecatapi.com/v1/images/search");
+
+            if (httpRM.IsSuccessStatusCode)
+            {
+                string result = await httpRM.Content.ReadAsStringAsync();
+                result = result.Replace("[", "").Replace("]", "");
+                ranCatApi = JsonConvert.DeserializeObject<RanCatApi>(result);
+            }
+            System.Diagnostics.Debug.Print(ranCatApi.url + " | " + ranCatApi.id);
+            return ranCatApi.url;
+        }
+    }
+    public class RanDogApi
+    {
+        public string message { get; set; }
+        public string status { get; set; }
+    }
+    public class RanDogApiHandler
+    {
+        private HttpClient httpC;
+        private HttpResponseMessage httpRM;
+        private RanDogApi ranDogApi = new RanDogApi();
+
+        public RanDogApiHandler()
+        {
+            httpC = new HttpClient();
+            httpRM = new HttpResponseMessage();
+        }
+
+        public async Task<string> GetImage()
+        {
+            httpRM = await httpC.GetAsync("https://dog.ceo/api/breeds/image/random");
+
+            if (httpRM.IsSuccessStatusCode)
+            {
+                string result = await httpRM.Content.ReadAsStringAsync();
+                ranDogApi = JsonConvert.DeserializeObject<RanDogApi>(result);
+            }
+            System.Diagnostics.Debug.Print(ranDogApi.message + " | " + ranDogApi.status);
+            return ranDogApi.message;
+        }
+    }
+    public class RanAnimeGirlApi
+    {
+        public string url { get; set; }
+    }
+    public class RanAnimeGirlApiHandler
+    {
+        private HttpClient httpC;
+        private HttpResponseMessage httpRM;
+        private RanAnimeGirlApi ranAnimeGirlApi = new RanAnimeGirlApi();
+
+        public RanAnimeGirlApiHandler()
+        {
+            httpC = new HttpClient();
+            httpRM = new HttpResponseMessage();
+        }
+        public async Task<string> GetImage(string type)
+        {
+            httpRM = await httpC.GetAsync("https://api.waifu.pics/sfw/" + type);
+            if (httpRM.IsSuccessStatusCode)
+            {
+                string result = await httpRM.Content.ReadAsStringAsync();
+                ranAnimeGirlApi = JsonConvert.DeserializeObject<RanAnimeGirlApi>(result);
+            }
+            System.Diagnostics.Debug.Print(ranAnimeGirlApi.url);
+            return ranAnimeGirlApi.url;
+        }
+    }
+}
--- JustARandomApp/Program.cs
+++ JustARandomApp/Program.cs
@@ -1,8 +1,12 @@
 using JustARandomApp;
+using JustARandomApp.Code;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddScoped<RanCatApiHandler>();
+builder.Services.AddScoped<RanDogApiHandler>();
+builder.Services.AddScoped<RanAnimeGirlApiHandler>();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
